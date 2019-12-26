483bee2df01a999792dacda3cb540ac63c1a609c
Myyy <1289903263@qq.com>
2019-12-26 14:19:38 +0800

    提交

---
+++ src/Fmain.java
@@ -0,0 +1,6 @@
+public class Fmain {
+    public static void main(System []args){
+        System.out.println("FZmain");
+        System.out.println("呵呵");
+    }
+}
---
+++ src/GitMain.java
@@ -0,0 +1,8 @@
+public class GitMain {
+    public static void main(System[] args){
+        System.out.println("dev分支啦啦啦啦");
+        System.out.println("master分支!");
+        System.out.println("master合并！");
+    }
+}
+
